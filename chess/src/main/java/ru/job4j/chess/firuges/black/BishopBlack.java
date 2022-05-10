d06a309e5df369f9560a3ebfd63605555fb2a193
boris86msk <boris86msk@yandex.ru>
2022-05-10 22:44:41 +0300

    BishopBlack 2

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -15,12 +15,26 @@ public class BishopBlack implements Figure {
     }
     @Override
     public Cell[] way(Cell dest) {
-        throw new ImpossibleMoveException(
-                String.format("Could not way by diagonal from %s to %s", position, dest)
-        );
+        if (!isDiagonal(position, dest)) {
+            throw new ImpossibleMoveException(
+                    String.format("Could not move by diagonal from %s to %s", position, dest)
+            );
+        }
+        int size = Math.abs(position.getX() - dest.getX());
+        Cell[] steps = new Cell[size];
+        int deltaX = (position.getX() - dest.getX()) / - size;
+        int deltaY = (position.getY() - dest.getY()) / - size;
+        int x = position.getX() + deltaX;
+        int y = position.getY() + deltaY;
+        for (int index = 0; index < size; index++) {
+            steps[index] = Cell.findBy(x, y);
+            x += deltaX;
+            y += deltaY;
+        }
+        return steps;
     }
     public boolean isDiagonal(Cell source, Cell dest) {
-        return false;
+        return Math.abs(source.getX() - dest.getX()) == Math.abs(source.getY() - dest.getY());
     }
     @Override
     public Figure copy(Cell dest) {
--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -22,4 +22,22 @@ public class BishopBlackTest {
         Cell expected = bishop2.position();
         assertThat(expected, is (Cell.C4));
     }
+
+    @Test
+    public void testWayF8toC5() {
+        Figure bishop = new BishopBlack(Cell.F8);
+        Cell[] way = bishop.way(Cell.C5);
+        Cell[] expected = {Cell.E7, Cell.D6, Cell.C5};
+        assertThat(expected, is (way));
+    }
+
+    @Test
+    public void testWayH3toC8() {
+        Figure bishop = new BishopBlack(Cell.H3);
+        Cell[] way = bishop.way(Cell.C8);
+        Cell[] expected = {Cell.G4, Cell.F5, Cell.E6, Cell.D7, Cell.C8};
+        assertThat(expected, is (way));
+    }
+
+
 }
