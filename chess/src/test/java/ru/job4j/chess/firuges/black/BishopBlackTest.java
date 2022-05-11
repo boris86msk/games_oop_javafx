c23736430f4fcbec1136379bbcf2c03727eae4a2
User <boris86msk@yandex.ru>
2022-05-11 19:09:15 +0300

    BishopBlack + Tests3

--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -13,7 +13,7 @@ public class BishopBlackTest {
     @Test
     public void testPosition() {
         Figure bishop = new BishopBlack(Cell.E4);
-        assertThat(Cell.E4, is (bishop.position()));
+        assertThat(Cell.E4, is(bishop.position()));
     }
 
     @Test
@@ -21,7 +21,7 @@ public class BishopBlackTest {
         Figure bishop = new BishopBlack(Cell.F1);
         Figure bishop2 = bishop.copy(Cell.C4);
         Cell expected = bishop2.position();
-        assertThat(expected, is (Cell.C4));
+        assertThat(expected, is(Cell.C4));
     }
 
     @Test
@@ -29,7 +29,7 @@ public class BishopBlackTest {
         Figure bishop = new BishopBlack(Cell.F8);
         Cell[] way = bishop.way(Cell.C5);
         Cell[] expected = {Cell.E7, Cell.D6, Cell.C5};
-        assertThat(expected, is (way));
+        assertThat(expected, is(way));
     }
 
     @Test
@@ -37,7 +37,7 @@ public class BishopBlackTest {
         Figure bishop = new BishopBlack(Cell.H3);
         Cell[] way = bishop.way(Cell.C8);
         Cell[] expected = {Cell.G4, Cell.F5, Cell.E6, Cell.D7, Cell.C8};
-        assertThat(expected, is (way));
+        assertThat(expected, is(way));
     }
 
     @Test(expected = ImpossibleMoveException.class)
