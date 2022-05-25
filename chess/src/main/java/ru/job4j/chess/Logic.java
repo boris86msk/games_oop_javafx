80e4e5c992fbdf4e2fdd8d3a61292f8c344394a0
User <boris86msk@yandex.ru>
2022-05-25 15:03:52 +0300

    BishopBlack + Tests 11

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -23,10 +23,7 @@ public final class Logic {
     private boolean free(Cell[] steps) throws OccupiedCellException {
         for (Cell step : steps) {
             for (Figure figure : figures) {
-                if (figure == null) {
-                    break;
-                }
-                if (figure.position().equals(step)) {
+                if (figure != null && figure.position().equals(step)) {
                     throw  new OccupiedCellException();
                 }
             }
